8eb5df0fbc5e7c454255407b7f0f5b48740bc120
ErikBot9 <erik.bot@mail.polimi.it>
2025-11-05 08:46:45 +0100

    Changed accuracy

--- R/scoreMapping.R
+++ R/scoreMapping.R
@@ -58,39 +58,15 @@ scoreMapping <- function(net,
   label_attr <- as.vector(label_attr)
 
   mean_cors <- t(get_column_group_average(t(new_cor), interaction(label_attr,
-                                                                  net$Stages, sep = "and")))
+                                                                  net$Stages)))
 
   accuracy <- apply(mean_cors, 2, max)
-  precision <- apply(mean_cors, 1, max)
+  highest_annotation <- rownames(mean_cors)[apply(mean_cors, 2, which.max)]
 
-  max_cor <- stats::cor(t(apply(as.matrix(SingleCellExperiment::logcounts(net)[rownames(net)[SingleCellExperiment::rowData(net)$informative],]), 1, function(v) {
-    (v - mean(v))/stats::sd(v)
-  })), as.matrix(SingleCellExperiment::logcounts(net)[rownames(net)[SingleCellExperiment::rowData(net)$informative],]))
-
-  max_mean_cors <- t(get_column_group_average(t(max_cor), interaction(label_attr,
-                                                                      net$Stages, sep = "and")))
-
-  max_accuracy <- mean(apply(max_mean_cors, 2, max))
-  accuracy <- accuracy/max_accuracy
-
-  max_precision <- apply(max_mean_cors, 1, max)
-  precision <- precision/max_precision
-
-  by_subclass_precision <- tapply(precision, unlist(lapply(strsplit(names(precision),
-                                                                    "and", fixed = TRUE), function(i) {i[1]})), mean)
-
-  by_stage_precision <- tapply(precision, unlist(lapply(strsplit(names(precision),
-                                                                 "and", fixed = TRUE), function(i) {i[2]})), mean)
-
-  accuracy[which(accuracy > 1)] <- 1
   global_accuracy <- mean(accuracy)
-  global_precision <- mean(precision)
-  mapping_score <- mean(c(global_accuracy, global_precision))
 
-  return(S4Vectors::List(ClusterAccuracy = accuracy,
-                         LabelPrecision = sort(by_subclass_precision),
-                         StagePrecision = sort(by_stage_precision),
-                         GlobalAccuracy = global_accuracy,
-                         GlobalPrecision = global_precision,
-                         MappingScore = mapping_score))
+  return(S4Vectors::List(
+    SampleHighestGroup = highest_annotation,
+    SampleAccuracy = accuracy,
+    GlobalAccuracy = global_accuracy))
 }
